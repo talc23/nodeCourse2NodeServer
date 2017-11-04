96e5b86423a5495bfe93bda350ad6f6499deda6d
Tal Cherckez <talc23@gmail.com>
2017-11-04 15:42:18 +0200

    Initial commit

---
+++ server.js
@@ -0,0 +1,64 @@
+const express = require('express')
+const hbs = require('hbs')
+const fs = require('fs')
+const allowedUrls = ['about', 'home', '/']
+
+var app = express()
+
+hbs.registerPartials(__dirname + '/views/partials')
+app.set('view engine', 'hbs')
+app.use(express.static(__dirname + '/public'))
+
+app.use((req, res, next)  => {
+  var now = new Date().toString()
+  var logMsg = `${now} - ${req.method} ${req.url}`
+  console.log(logMsg)
+  fs.appendFile('server.log', logMsg + '\n', (err) =>{
+    if(err){
+      console.log('Unable to append to server.log')
+    }
+  })
+  next()
+})
+
+app.use((req, res, next)  => {
+  if ((req.method === 'GET') && ((req.url==='/home') || (req.url==='/about')|| (req.url==='/')))
+  {
+    next()
+  }
+  res.render('maintenance.hbs' , {
+  })
+})
+
+hbs.registerHelper('getCurYear', () =>{
+  return new Date().getFullYear()
+})
+
+hbs.registerHelper('screamIt', (text) =>{
+  return text.toUpperCase()
+})
+
+app.get('/', (req, res) => {
+  // res.send('<h1>hello Express</h1>')
+  res.render('home.hbs' , {
+    pageTitle: 'Home Page',
+    welcomeMessage: "Welcome to my website",
+    likes: ['Nothing', 'cooking']
+  })
+})
+
+app.get('/about', (req, res) => {
+  res.render('about.hbs', {
+    pageTitle: 'About Page'
+  })
+})
+
+app.get('/bad', (req, res) => {
+  res.send({
+    errorMessage: `Unable to handle request`
+  })
+})
+
+app.listen(3000, () =>{
+  console.log('Server is up on port 3000')
+})
